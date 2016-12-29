d5f84668ce342955d7e496b212f6a968ae8d5876
zx1340 <phambao@outlook.com>
2016-12-29 11:31:25 +0700

    Wrong folder XD

--- client.py
+++ client.py
@@ -1,15 +1,28 @@
 import socks
 import socket
 
-PORT = 8080
-HOST = socket.gethostname()
+MAX_PACKAGE_SIZE = 99999
 
-socks.setdefaultproxy(socks.PROXY_TYPE_SOCKS4, "127.0.0.1", PORT)
-socket.socket = socks.socksocket
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((HOST, PORT))
+def cn():
+    PORT = 8080
+    HOST = socket.gethostname()
+    socks.setdefaultproxy(socks.PROXY_TYPE_SOCKS4, "127.0.0.1", PORT)
+    socket.socket = socks.socksocket
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect(('127.0.0.1', PORT))
+    return s
 
-#name = raw_input("What is your command? ")
 
-#s.send("print 357")
-#s.close()
+
+s = cn()
+while True:
+    x = raw_input(">")
+    try:
+        s.send(x)
+        print(s.recv(MAX_PACKAGE_SIZE))
+    except:
+        print('Connection fail\nPLS exit and try again....')
+
+s.close()
+
+
