1caf2ca4047f8edb52e4f6e6f691d663dc4ad276
zx1340 <phambao@outlook.com>
2016-12-29 11:28:19 +0700

    FIxed error on linux
    Now it support both window and linux.

--- client.py
+++ client.py
@@ -1,27 +1,15 @@
 import socks
 import socket
 
-MAX_PACKAGE_SIZE = 99999
-def cn():
-    PORT = 8080
-    HOST = socket.gethostname()
-    socks.setdefaultproxy(socks.PROXY_TYPE_SOCKS4, "127.0.0.1", PORT)
-    socket.socket = socks.socksocket
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.connect((HOST, PORT))
-    return s
+PORT = 8080
+HOST = socket.gethostname()
 
+socks.setdefaultproxy(socks.PROXY_TYPE_SOCKS4, "127.0.0.1", PORT)
+socket.socket = socks.socksocket
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((HOST, PORT))
 
+#name = raw_input("What is your command? ")
 
-s = cn()
-while True:
-    x = raw_input(">")
-    try:
-        s.send(x)
-        print s.recv(MAX_PACKAGE_SIZE)
-    except:
-        print 'Connection fail]\nTry again....'
-
-s.close()
-
-
+#s.send("print 357")
+#s.close()
--- socks4.py
+++ socks4.py
@@ -12,37 +12,9 @@ from termcolor import colored
 import sys
 import getopt
 import colorama
-import socket
-
 
 port, logfile, dumplen, printmode, hijack, olddata, newdata, ignore = 9999, '', 16, 'hex', '', '', '', ''
 colorama.init()
-
-
-local_ip = socket.gethostbyname(socket.gethostname())
-
-
-
-
-class Packet():
-    def __init__(self,package,Mode,data):
-        self.package = package
-        self.mode = Mode
-        self.data = data
-
-
-
-
-
-class Datahandle():
-    def __init__(self):
-        self.allpk = {}
-    
-    def addpk(self,package,Mode,data):
-        self.allpk[str(len(self.allpk))] = Packet(package,Mode,data)
-
-
-
 
 
 def hexdump(src, sep='.'):
@@ -61,8 +33,6 @@ def hexdump(src, sep='.'):
             ['%s' % ((ord(x) <= 127 and FILTER[ord(x)]) or sep) for x in chars])
         lines.append('%08x:  %-*s  %s\n' % (c, length * 3, hex, printable))
     return ''.join(lines)
-
-
 
 
 def logdata(data):
@@ -101,7 +71,7 @@ def main(argv):
     global port, logfile, dumplen, printmode, hijack, olddata, newdata, ignore
 
     try:
-        myopts, args = getopt.getopt(sys.argv[1:], 'p:l:x:t')
+        myopts, args = getopt.getopt(sys.argv[1:], 'p:l:x:t:h:i:')
     except getopt.GetoptError as e:
         print (str(e))
         print 'Usage: %s [options]\nOptions:' \
@@ -109,6 +79,8 @@ def main(argv):
               '\n-l logfile (traffic log)' \
               '\n-x hexlength (hex length to print, default is 16)' \
               '\n-t text/hex (display mode: text or hex'\
+              '\n-h s/r:olddata:newdata (data replacement, s is send, r is recv)'\
+              '\n-i s/h:ignore (ignore for replacement, h for hex input'\
               '\n' % (sys.argv[0])
         sys.exit(2)
 
@@ -133,330 +105,136 @@ def main(argv):
     reactor.run()
 
 
+class Packet():
+    def __init__(self,package,data):
+        self.package = package
+        self.data = data
 
 
-def rpl(string,position,value):
-    return string[0:position] + value + string[position + 1:]
 
-
-
-
-def strdiff(x,y):
-
-    if len(x) != len(y):
-        return 'Cannot compare two diffirent package size'
-
-    hx = hexdump(x)
-    hy = hexdump(y)
-
-
-    diff = [i for i in xrange(len(hx)) if hx[i] != hy[i]]
-
-    rx = ''
-    ry = ''
-    for i in diff:
-        if i % 77 < 58:
-            if hx[i+1] == ' ' and (i-1) not in diff:
-                diff.append(i-1)
-            elif hx[i-1] == ' ' and (i+1) not in diff:
-                diff.append(i+1)
-            else:
-                pass
-
-    for i in range(len(hx)):
-        if i in diff:
-            rx += colored(hx[i],'red')
-            ry += colored(hy[i],'red')
-        else:
-            rx += hx[i]
-            ry += hy[i]
-
-    return rx + '\n' + ry
-
-
-def changeFilter(command):
-    global Filter
+class Datahandle():
+    def __init__(self):
+        self.allpk = {}
     
-        #filter addport 443
-    if command[1] == 'info':
-        return str(Filter)
+    def addpk(self,package,data):
+        self.allpk[str(len(self.allpk))] = Packet(package,data)
 
 
-    if command[1] == 'addport':
-        if command[2] in Filter[port]:
-            return '[I]This port already filted'
-        else:
-            Filter[port].append(command[2])
-            return '[I]Port %s added successfull' % (command[2])
-
-    if command[1] == 'removeport':
-        if command[2] not in Filter[port]:
-            return '[E]Need add port before remove it'
-        else:
-            Filter[port].remove(command[2])
-            return '[I]Remove port successfull'
-
-    if command[1] == 'ban':
-        if command[2] in Filter['BanIP']:
-            return '[I]Ip already banned'
-
-
-
-    if command[1] == 'len':
-
-        try:
-            int(command[2])
-        except:
-            return '[E]Invalid package size'
-
-
-
-        if command[2] not in Filter['Size']:
-            Filter['Size'].append(command[2])
-        return '[I]Current filter package size: %s ' % (str(Filter['Size']))
-
-    if command[1] == 'sendonly':
-        Filter['Mode'] = 'Send'
-        return '[I]Mode Send turned on'
-    if command[1] == 'recvonly':
-        Filter['Mode'] = 'Recv'
-        return '[I]Mode recv turned on'
-
-    if command[1] == 'nofilter':
-        Filter = {
-        'Mode':None,
-        'Port':[],
-        'Size':[],
-        'BanIP':[]
-        }
-        return '[I]No Filter now'
-    return '[E]Unidentified filter'
-
-
-
-def using():
-    return '[-]repeat <package_id>\n[-]print <package_id>\n[-]diff <package_id_1> <package_id_2>\n[-]replace <package_id> <location> <value>\n[-]send <package_id> <encode_hex_string>\n[-]filter [addport|removeport|sendonly|recvonly] + [port]'
 
 
 class MSock4(SOCKSv4):
 
-
-
-    def handlePackage(self,Mode,data):
-        #check filter and print out
-        
-        global datahandle
-
-        datahandle.addpk(self,Mode,data)
-
-        csinfo = getCSInfo(self)
-
-        if Filter['Mode'] == 'Send' and Mode == 'Send': return False
-        if Filter['Mode'] == 'Recv' and Mode == 'Recv': return False
-        if csinfo[0] in Filter['BanIP'] or csinfo[2] in Filter['BanIP']: return False
-
-        if len(Filter['Size']):
-            if str(len(data)) not in Filter['Size']:
-                return False
-
-        prtcsinfo = '%s:%s <-- %s:%s' % csinfo
-        prtcsinfo += ' len: %s' % len(data)
-        prtcsinfo += ' id: %s' % (len(datahandle.allpk) - 1)
-
-
-        print colored(prtcsinfo, 'red')
-        print colored(hexdump(data), 'yellow') if Mode == 'Send' else colored(hexdump(data), 'green')
-        
-
-
-
-
-    def runCommand(self,cmd):
-        
-        ret = ()
-        
-        print '[Excute]',cmd
-        
+    def runCommand(self,cmd,datahandle):
+        print 'calc command',cmd,len(cmd.split(' '))
         if len(cmd.split(' ')) < 2:
-            #print '[E] Command error'
-            return '[E]Unidentified command\n' + using()
-        
-        command = cmd.split(' ')
+            print '[E] Command error'
 
-        if command[0] == 'repeat':
-            if len(command) != 2:
-                return '[E]Using: repeat <package_id>'
+        if len(cmd.split(' ')) == 2:
+            command = cmd.split(' ')[0]
+            args = cmd.split(' ')[1]
+            if command == 'repeat':
+
+                repeat_package = datahandle.allpk[args].package
+                print '[REPEAT]',getCSInfo(repeat_package),datahandle.allpk[args].data
+                SOCKSv4.dataReceived(repeat_package,datahandle.allpk[args].data)
+
+            elif command == 'print':
+                print_package = datahandle.allpk[args].package
+
+                print '[PRINT]',getCSInfo(print_package)
+                data = datahandle.allpk[args].data
+                print colored(hexdump(data), 'red')
 
             else:
-                pckg_id = command[1]
-                try:
-                    if int(pckg_id) >= len(datahandle.allpk):
-                        return '[E]Invalid package id, current %s' %(len(datahandle.allpk) - 1) 
-                except:
-                    return '[E]Invalid package id'
-
-                repeat_package = datahandle.allpk[pckg_id].package
-                data = datahandle.allpk[pckg_id].data
-                
-                SOCKSv4.dataReceived(repeat_package,datahandle.allpk[pckg_id].data)
-
-                return str(getCSInfo(repeat_package)) + '\n' +  str(colored(hexdump(data), 'red'))
-
-        elif command[0] == 'print':
-            if len(command) != 2:
-                return '[E]Using: print <package_id>' 
-            else:
-                
-                pckg_id = command[1]
-                try:
-                    if int(pckg_id) >= len(datahandle.allpk):
-                        return '[E]Invalid package id, current %s' %(len(datahandle.allpk) - 1) 
-                except:
-                    return '[E]Invalid package id'
-
-                print_package = datahandle.allpk[pckg_id].package
-
-                data = datahandle.allpk[pckg_id].data
-
-                return str(getCSInfo(print_package)) + '\n' +  str(colored(hexdump(data), 'red')) + '\n'
-        
-        elif command[0] == 'get':
-            if len(command) != 2:
-                return '[E]Using: print <package_id>' 
-            else:
-                pckg_id = command[1]
-                try:
-                    if int(pckg_id) >= len(datahandle.allpk):
-                        return '[E]Invalid package id, current %s' %(len(datahandle.allpk) - 1) 
-                except:
-                    return '[E]Invalid package id'
-                print_package = datahandle.allpk[pckg_id].package
-                data = datahandle.allpk[pckg_id].data
-                return str(getCSInfo(print_package)) + '\n' + data.encode('hex')
+                print "[E] Not implement"
 
 
-        elif command[0] == 'diff':
-            if len(command) != 3:
-                return '[E]Using: diff <package_id_1> <package_id_2>'
-            else:
-                pckg1_id = cmd.split(' ')[1]
-                pckg2_id = cmd.split(' ')[2]
-                try:
-                    if int(pckg1_id) >= len(datahandle.allpk) or int(pckg2_id) >= len(datahandle.allpk):
-                        return '[E]Invalid package id, current %s' %(len(datahandle.allpk) - 1) 
-                except:
-                    return '[E]Invalid package id'
+        if len(cmd.split(' ')) == 3:
+            command = cmd.split(' ')[0]
+            arg1 = cmd.split(' ')[1]
+            arg2 = cmd.split(' ')[2]
 
-                pckg1_data = datahandle.allpk[pckg1_id].data
-                pckg2_data = datahandle.allpk[pckg2_id].data
-                ret = strdiff(pckg1_data,pckg2_data)
-                return ret
+            if command == 'compare':
+                pckg1_data = datahandle.allpk[arg1].data
+                pckg2_data = datahandle.allpk[arg2].data
+                #for i in pckg1_data:
 
 
-        elif command[0] == 'replace':
-            if len(command) != 4:
-                return '[E]Using: replace <package_id> <location> <value>'
-            else:
-                
-                pckg_id = command[1]
-
-                try:
-                    if int(pckg_id) >= len(datahandle.allpk):
-                        return '[E]Invalid package id, current %s' %(len(datahandle.allpk) - 1)
-
-                except:
-                    return '[E]Invalid package id'
 
 
-                location = command[2]
-
-                value = command[3]
 
 
-                rpl_pckg = datahandle.allpk[pckg_id].package
-
-                data = datahandle.allpk[pckg_id].data
-
-                data = rpl(data,int(location),chr(int('0x' + value,16)))
-
-                SOCKSv4.dataReceived(rpl_pckg,data)
-                
-                return colored(hexdump(data),'green')
-        
-        elif command[0] == 'send':
-            if len(command) != 3:
-                return '[E]Using: send <package_id> <encode_hex_string>'
-            else:
-
-                pckg_id = command[1]
-
-                try:
-                    if int(pckg_id) >= len(datahandle.allpk):
-                        return '[E]Invalid package id, current %s' %(len(datahandle.allpk) - 1)
-
-                except:
-                    return '[E]Invalid package id'
-
-                data = command[2].decode('hex')
-                
-                rpl_pckg = datahandle.allpk[pckg_id].package
-
-                SOCKSv4.dataReceived(rpl_pckg,data)
-                return '[I]Packet send without error'
-
-
-        elif command[0] == 'filter':
-            if len(command) != 2 and len(command) != 3:
-                return '[E]Using: filter [addport|removeport|sendonly|recvonly] + [port]'
-            else:
-                return changeFilter(command)
-
-        else:
-            return '[E]Unidentified command\n' + using()
+            print "[E] Not implement"
+        return
 
 
     def dataReceived(self, data):
+        global datahandle
 
         if not self.otherConn:
             return SOCKSv4.dataReceived(self, data)
 
+        if hijack == 's':
+            data = editdata(data)
+
         
         cs = getCSInfo(self)
+        if cs[0] == '127.0.0.1':
+            print '[>] Excute command:',data
+        
+            self.runCommand(data,datahandle)
+            
 
-        if cs[0] == '127.0.0.1' and cs[2] == local_ip:
-            print '[<] Excute command:',data
-            ret = self.runCommand(data)
-            return SOCKSv4.write(self,ret)
+            return SOCKSv4.dataReceived(self,data)
+        
 
+        datahandle.addpk(self,data)
+        #return SOCKSv4.dataReceived(self,data)
+        
+        csinfo = '%s:%s --> %s:%s' % getCSInfo(self)
+        csinfo += ' len: %s' % len(data)
+        print colored(csinfo, 'red')
+        print colored(hexdump(data), 'green')
 
-        self.handlePackage('Send',data)
+        logdata('%s\n%s' % (csinfo, hexdump(data)))
+        
+
+        print '[TOTAL PACKAGE]', len(datahandle.allpk)
         return SOCKSv4.dataReceived(self, data)
 
 
     def write(self, data):
+        global datahandle
+
         
+
         if not self.otherConn:
             return SOCKSv4.write(self, data)
 
         if hijack == 'r':
             data = editdata(data)
         
-        
-        self.handlePackage('Recv',data)
-        
+        cs = getCSInfo(self)
+
+        datahandle.addpk(self,data)
+        return SOCKSv4.write(self, data)
+
+        csinfo = '%s:%s <-- %s:%s' % getCSInfo(self)
+        csinfo += ' len: %s' % len(data)
+        print colored(csinfo, 'red')
+        print colored(hexdump(data), 'yellow')
+
+
+        logdata('%s\n%s' % (csinfo, hexdump(data)))
+
+        print '[TOTAL PACKAGE]', len(datahandle.allpk)
 
         return SOCKSv4.write(self, data)
 
 
 
-
 datahandle = Datahandle()
-Filter = {
-    'Mode':None,
-    'Port':[],
-    'Size':[],
-    'BanIP':[]
-}
+filter = '49.213.119.232'
 
 if __name__ == '__main__':
     main(sys.argv)
